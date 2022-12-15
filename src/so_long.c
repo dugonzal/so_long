9fca7ee1acf62d68cf6b8199ca2f97a9b9cc85f8
dugonzal <ciclocontinuo2@gmail.com>
2022-12-15 23:10:04 +0100

    printiando la ruta del mapa pasada por parametro

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:01 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/15 23:02:47 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/15 23:08:34 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 
 int	main(int ac, char **av)
 {
-	t_game game;
+	t_game *game;
 
 
 	if (ac == 2)
 	{
-		init_map (av[1]);
+		game = init_map (av[1]);
+		printf ("%s", game->map.file_d);
 		//game.mlx = mlx_init();
 		//game.mlx_win = mlx_new_window(game.mlx, 1000, 800, "so_long");
 		//mlx_loop(game.mlx);
