7575f6958a8e4176b23f6ada72a434f684dbff07
dugonzal <ciclocontinuo2@gmail.com>
2022-12-21 21:40:39 +0100

    ancho y altura de la ventana de forma dinamica dependiendo del tamaño del mapa

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:01 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/15 23:08:34 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/21 21:39:38 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 
 int	main(int ac, char **av)
 {
-	t_game *game;
-
+	t_game game;
 
 	if (ac == 2)
 	{
-		game = init_map (av[1]);
-		printf ("%s", game->map.file_d);
-		//game.mlx = mlx_init();
-		//game.mlx_win = mlx_new_window(game.mlx, 1000, 800, "so_long");
-		//mlx_loop(game.mlx);
+		init_map (av[1], &game);
+		len_map (&game);
+		game.mlx = mlx_init();
+		game.mlx_win = mlx_new_window(game.mlx, game.map.width * 30, game.map.height * 50, "so_long");
+		mlx_loop(game.mlx);
 	}
 	return (0);
 }
