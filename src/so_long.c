924080b5204a74ee9db4f2dbea531f6e07348621
dugonzal <ciclocontinuo2@gmail.com>
2022-12-22 17:32:02 +0100

    lectura correcta del mapa

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:01 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/21 22:07:29 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/22 17:31:35 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,6 @@ int	main(int ac, char **av)
 		init_map (av[1], &game);
 		len_map (&game);
 		read_map (&game);
-		int i = 0;
-		while (game.map.map)
-		{
-			printf ("%s", game.map.map[i]);
-			i++;
-		}
-
-
 		//game.mlx = mlx_init();
 		//game.mlx_win = mlx_new_window(game.mlx, game.map.width * 30, game.map.height * 50, "so_long");
 		//mlx_loop(game.mlx);
