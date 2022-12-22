15e2fee6d42596976a37d10afb5ea47f7d39c9e2
dugonzal <ciclocontinuo2@gmail.com>
2022-12-22 14:22:33 +0100

    cambios cosmicos xd

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:26 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/15 23:07:05 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/21 21:57:16 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@
 #include "../libft/include/libft.h"
 #include "../mlx_linux/mlx.h"
 
+# define ESC 53
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+
 typedef struct s_map
 {
 	char	**map;
 	int		width;
-	char	*file_d;
 	int		height;
+	char	*file_d;
 }	t_map;
 
 typedef struct s_player
@@ -29,6 +35,7 @@ typedef struct s_player
 	int		x;
 	int		y;
 	int		collectibles;
+	int		scape;
 }	t_player;
 
 typedef struct s_game
@@ -39,7 +46,8 @@ typedef struct s_game
 	t_player	player;
 }	t_game;
 
-t_game	*init_map(char *map);
-
-
+void	init_map(char *file_d, 	t_game *game);
+void	len_map(t_game *game);
+void	read_map(t_game *game);
+void	err(char *s);
 #endif
