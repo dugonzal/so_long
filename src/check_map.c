ca4cf564136158ba67ebde266d6a9c7243dd84ba
dugonzal <ciclocontinuo2@gmail.com>
2022-12-23 23:29:19 +0100

    norminette done

--- src/check_map.c
+++ src/check_map.c
@@ -6,28 +6,24 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:19:38 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/23 21:36:18 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/23 23:28:51 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-
 void	check_map(t_game *game)
 {
-	int i;
-	int err;
-	int len;
+	int	i;
+	int	err;
 
 	i = 0;
 	err = 0;
-	len = arr_size (game->map.map) -1;
 	while (game->map.width--)
 	{
-		if (game->map.map[0][i] != '1' || game->map.map[len][i] != '1')
+		if (game->map.map[0][i] != '1' || \
+		game->map.map[arr_size (game->map.map) - 1][i] != '1')
 			err++;
-
-		printf ("%c",game->map.map[len][i]);
 		i++;
 	}
 	printf ("[%d]", err);
