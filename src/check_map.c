9b652298147f36a231976390158523a88857bbe2
dugonzal <ciclocontinuo2@gmail.com>
2022-12-23 21:34:42 +0100

    en el header aprace lshonta lol

--- src/check_map.c
+++ src/check_map.c
@@ -6,13 +6,13 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:19:38 by lshonta           #+#    #+#             */
-/*   Updated: 2022/12/23 21:28:42 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/23 21:34:19 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-
+// quien es lshonta wtf aparece en el header lol
 void	check_map(t_game *game)
 {
 	int i;
@@ -22,7 +22,6 @@ void	check_map(t_game *game)
 	i = 0;
 	err = 0;
 	len = arr_size (game->map.map) -1;
-	printf ("[%d]\n", len);
 	while (game->map.width--)
 	{
 		if (game->map.map[0][i] != '1' || game->map.map[len][i] != '1')
