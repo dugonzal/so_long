7eb143a1b0bf68feb49b97018bb8a623f5458bee
dugonzal <ciclocontinuo2@gmail.com>
2022-12-24 00:23:17 +0100

    compruebo los muros de la linea 0, la ultima linea no se compueba el longitud no concuerda

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:19:38 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/23 23:28:51 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/24 00:21:15 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,26 @@
 
 void	check_map(t_game *game)
 {
-	int	i;
+	int	v;
 	int	err;
 
-	i = 0;
+	v = 0;
 	err = 0;
 	while (game->map.width--)
 	{
-		if (game->map.map[0][i] != '1' || \
-		game->map.map[arr_size (game->map.map) - 1][i] != '1')
+		if (game->map.map[0][v] != '1' || \
+		game->map.map[arr_size (game->map.map) - 1][v] != '1')
 			err++;
-		i++;
+		v++;
+	}
+	v = 0;
+	int len = arr_size (game->map.map)-1;
+	while (--len)
+	{
+		if (game->map.map[v][0] != '1' || game->map.map[v][ft_strlen(game->map.map[0])-3])
+			err++;
+		v++;
+		printf ("%c", game->map.map[v][ft_strlen(game->map.map[0])-2]);
 	}
 	printf ("[%d]", err);
 	if (err > 1)
