d7305b9d210d3b99066965d1edd182645636f3fd
dugonzal <ciclocontinuo2@gmail.com>
2022-12-24 18:06:14 +0100

    acorto funcion de comprobacion de caracteres y corrijo los fallos de normintte

--- src/check_map.c
+++ src/check_map.c
@@ -6,17 +6,24 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:19:38 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/24 17:53:51 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/24 18:05:30 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+static int	check(t_game *game, int i, int n)
+{
+	return (game->map.map[i][n] == '1' || game->map.map[i][n] == '0' \
+			|| game->map.map[i][n] == 'C' || game->map.map[i][n] == '\n' || \
+			game->map.map[i][n] == 'E' || game->map.map[i][n] == 'P' );
+}
+
 void	check_characters(t_game *game)
 {
-	int i;
-	int n;
-	int p;
+	int	i;
+	int	n;
+	int	p;
 
 	i = 0;
 	p = 0;
@@ -25,11 +32,10 @@ void	check_characters(t_game *game)
 		n = 0;
 		while (game->map.map[i][n])
 		{
-			if (game->map.map[i][n] == '1' || game->map.map[i][n] == '0' \
-			|| game->map.map[i][n] == 'C' || game->map.map[i][n] == '\n' || \
-			game->map.map[i][n] == 'E' || game->map.map[i][n] == 'P' )
+			if (check (game, i, n))
 			{
-				if (game->map.map[i][n - 1] == 'E' || game->map.map[i][n - 1] == 'P')
+				if (game->map.map[i][n - 1] == 'E' || \
+				game->map.map[i][n - 1] == 'P')
 					p++;
 				n++;
 			}
@@ -39,29 +45,29 @@ void	check_characters(t_game *game)
 		i++;
 	}
 	if (p != 2)
-	errors ("hay mas de un avatar o salidas en el mapa");
+		errors ("hay mas de un avatar o salidas en el mapa");
 }
-
 
 void	check_map(t_game *game)
 {
-	int	v;
-	int	err;
-	size_t len;
+	int		v;
+	int		err;
+	size_t	len;
 
 	v = 0;
 	err = 0;
 	len = arr_size (game->map.map) - 1;
-	while (v < game->map.width)// anchura
+	while (v < game->map.width)
 	{
 		if (game->map.map[0][v] != '1' || game->map.map[len][v] != '1')
 			err++;
 		v++;
 	}
 	v = 0;
-	while (++v < game->map.height) // altura
+	while (++v < game->map.height)
 	{
-		if (game->map.map[v][0] != '1' || game->map.map[v][game->map.width-2] != '1')
+		if (game->map.map[v][0] != '1' || \
+		game->map.map[v][game->map.width - 2] != '1')
 			err++;
 	}
 	if (err > 1)
