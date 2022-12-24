7edf896e946da8f86efa046f03ba70b7b425d270
dugonzal <ciclocontinuo2@gmail.com>
2022-12-24 12:47:34 +0100

    si iteramos una variable de la estrcutura cambia su valor, mejor comparar y no iterar si no queremos cambiar el valor de la variable en la estructura

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:19:38 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/24 00:21:15 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/24 12:46:20 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,25 @@ void	check_map(t_game *game)
 {
 	int	v;
 	int	err;
+	size_t len;
 
 	v = 0;
 	err = 0;
-	while (game->map.width--)
+	len = arr_size (game->map.map) - 1;
+	while (v < game->map.width)// anchura
 	{
-		if (game->map.map[0][v] != '1' || \
-		game->map.map[arr_size (game->map.map) - 1][v] != '1')
+		if (game->map.map[0][v] != '1' || game->map.map[len][v] != '1')
 			err++;
 		v++;
 	}
 	v = 0;
-	int len = arr_size (game->map.map)-1;
-	while (--len)
+	while (++v < game->map.height) // altura
 	{
-		if (game->map.map[v][0] != '1' || game->map.map[v][ft_strlen(game->map.map[0])-3])
+		if (game->map.map[v][0] != '1' || game->map.map[v][game->map.width] != '1')
 			err++;
-		v++;
-		printf ("%c", game->map.map[v][ft_strlen(game->map.map[0])-2]);
 	}
+	printf ("[%d]\n", game->map.width);
+	printf ("{%c}",game->map.map[0][game->map.width]-2);
 	printf ("[%d]", err);
 	if (err > 1)
 		errors ("");
