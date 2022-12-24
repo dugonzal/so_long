77a114dde4ac436172d1bce0550ca76150257065
dugonzal <ciclocontinuo2@gmail.com>
2022-12-24 15:01:37 +0100

    comprobacion que el mapa solo este compuesto por los 5 caracteres especificados

--- src/check_map.c
+++ src/check_map.c
@@ -6,11 +6,46 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 23:19:38 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/24 12:52:25 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/24 15:00:05 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+
+void	check_characters(t_game *game)
+{
+	int i;
+	int n;
+	int p;
+
+	i = 0;
+	p = 0;
+	while (game->map.map[i])
+	{
+		n = 0;
+		while (game->map.map[i][n])
+		{
+			if (game->map.map[i][n] == '1' || game->map.map[i][n] == '0' \
+			|| game->map.map[i][n] == 'C' || game->map.map[i][n] == '\n' || \
+			game->map.map[i][n] == 'E' || game->map.map[i][n] == 'P' )
+			{
+				if (game->map.map[i][n - 1] == 'E' || game->map.map[i][n - 1] == 'P')
+					p++;
+				n++;
+			}
+			else
+			{
+				printf ("[%c]\n", game->map.map[i][n]);
+				errors ("hay carateres no contemplados en el mapa");
+			}
+			n++;
+		}
+		i++;
+	}
+	if (p != 2)
+	errors ("hay mas de un avatar o salidas en el mapa");
+}
+
 
 void	check_map(t_game *game)
 {
@@ -35,4 +70,5 @@ void	check_map(t_game *game)
 	}
 	if (err > 1)
 		errors ("Map periferia != 1");
+	check_characters (game);
 }
