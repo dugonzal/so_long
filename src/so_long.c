0a98e8fd2c6404df308813550955d3e2d8530d12
dugonzal <ciclocontinuo2@gmail.com>
2022-12-24 17:53:14 +0100

    mensaje de error cuando no se pasa un mapa o es diferente de 2

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:01 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/24 11:51:08 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/24 17:49:19 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,9 @@ int	main(int ac, char **av)
 		len_map (&game);
 		read_map (&game);
 		check_map (&game);
-
 	}
+	else
+		errors ("no se ha especificado un mapa");
 		//game.mlx = mlx_init();
 		//game.mlx_win = mlx_new_window(game.mlx, game.map.width * 30, game.map.height * 50, "so_long");
 		//mlx_loop(game.mlx);
