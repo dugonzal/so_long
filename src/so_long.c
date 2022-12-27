c2acd620a0e152816d395434d0250f57ab0bd6fc
dugonzal <ciclocontinuo2@gmail.com>
2022-12-27 14:15:58 +0100

    organizo temporalmente las funciones del main

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:01 by ciclo             #+#    #+#             */
-/*   Updated: 2022/12/26 18:09:44 by ciclo            ###   ########.fr       */
+/*   Updated: 2022/12/27 14:14:53 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,20 @@ static int	create_map(t_game *game)
 	return (0);
 }
 */
+void	funcions(t_game *game, char *path)
+{
+	init_structs (path, game);
+	len_map (game);
+	read_map (game);
+	check_map (game);
+	check_multi (game);
+}
 
+void events(t_game *game)
+{
+	mlx_hook(game->mlx_win, 17, 1L<<0, close_window, game);// con este loop o bucle podemos cerra la ventana desde la x
+	mlx_hook(game->mlx_win, 2, 1, key_press, game);
+}
 
 
 int	main(int ac, char **av)
@@ -46,15 +59,10 @@ int	main(int ac, char **av)
 
 	if (ac != 2)
 		errors ("no se ha especificado un mapa");
-	init_structs (av[1], &game);
-	len_map (&game);
-	read_map (&game);
-	check_map (&game);
-	check_multi (&game);
+	funcions (&game, av[1]);
 	game.mlx = mlx_init();
 	game.mlx_win = mlx_new_window(game.mlx, game.map.width * 30, game.map.height * 50, "so_long");
-	mlx_hook(game.mlx_win, 17, 1L<<0, close_window, &game);// con este loop o bucle podemos cerra la ventana desde la x
-	mlx_hook(game.mlx_win, 2, 1, key_press, &game);
+	events (&game);
 	mlx_loop(game.mlx);
 	exit (EXIT_SUCCESS);
 }
