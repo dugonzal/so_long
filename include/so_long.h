5a6b43a05f498019d401c602fd2ea8358c8fd990
Duvan Gonzalez <dugonzal@c3r5s3.42urduliz.com>
2023-01-24 19:30:36 +0100

    agrego la tecla Q en hexadecimal en la definicion de la Q

--- include/so_long.h
+++ include/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:26 by ciclo             #+#    #+#             */
-/*   Updated: 2023/01/22 22:13:30 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/01/24 19:28:58 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@
 # define SO_LONG_H
 
 #include "../libft/include/libft.h"
-#include "../mlx_linux/mlx.h"
+#include "../minilibx_macos/mlx.h"
 
-# define Q 53
+# define Q 0x0C  // 12
 # define W 13
 # define A 0
 # define S 1
 # define D 2
 
-#define avatar "./test.xpm"
+#define	black "images/black.xpm"
+#define	walls "images/wall.xpm"
+#define	portal "images/portal.xpm"
+#define coin	"images/coin.xpm"
+#define avatar "images/ava.xpm"
+//necesito una estructura para guradar los tamaños de la imagenes por separado, icluir esa lista en laestructura de imagenes
 typedef struct s_map
 {
 	char	**map;
@@ -41,6 +46,15 @@ typedef struct s_player
 	int		scape;
 }	t_player;
 
+
+typedef struct s_size
+{
+	int y_player;
+	int x_player;
+	int y_wall;
+	int	x_wall;
+}t_size;
+
 typedef struct s_image
 {
 	void	*collect;
@@ -50,14 +64,13 @@ typedef struct s_image
 	void	*exit;
 	int		width;
 	int		height;
+	t_size	size;
 }	t_image;
-
 
 typedef struct s_game
 {
 	void		*mlx;
 	void		*mlx_win;
-	void		*n;
 	t_map		map;
 	t_image		img;
 	t_player	player;
@@ -72,7 +85,10 @@ void	assign_caracteres_map(t_game *game);
 int		close_window(t_game *game);
 int		key_press(int keycode, t_game *game);
 void	map_images(t_game *game);
-int		create_map(t_game *game);
+void	create_map(t_game *game);
+
+//funcion chorra para imrpimir
+void print_game(t_game *game);
 
 
 #endif
