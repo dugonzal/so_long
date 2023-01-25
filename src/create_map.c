444dd91e0569ba65407d3968361c7d2b5efdb27b
Duvan Gonzalez <dugonzal@c3r3s6.42urduliz.com>
2023-01-25 12:57:47 +0100

    agrego sprites al mapa
    hasta ahora los [muros, espacio, colleccionables y salida] se pintan
    bien en la pantalla en los direntes mapas hay un un poco de margen en la
    parte derecha

--- src/create_map.c
+++ src/create_map.c
@@ -6,7 +6,7 @@
 /*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 08:57:56 by dugonzal          #+#    #+#             */
-/*   Updated: 2023/01/24 15:13:43 by dugonzal         ###   ########.fr       */
+/*   Updated: 2023/01/25 12:57:32 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,10 @@ eso hace que al empujarlo las imagenes a pantalla haya conflicto entre tamaños 
 */
 void	map_images(t_game *game)
 {
-	int i;
-	int j;
-	
-	game->img.floor = mlx_xpm_file_to_image(game->mlx, black, &game->img.width, &game->img.height);
+	game->img.floor = mlx_xpm_file_to_image(game->mlx, floors, &game->img.size.x_floor, &game->img.size.y_floor);
 	game->img.wall = mlx_xpm_file_to_image(game->mlx, walls, &game->img.size.x_wall, &game->img.size.y_wall);
-	game->img.exit = mlx_xpm_file_to_image(game->mlx, portal, &game->img.width, &game->img.height);
-	game->img.collect = mlx_xpm_file_to_image(game->mlx, coin, &(j), &(i));
+	game->img.exit = mlx_xpm_file_to_image(game->mlx, portal, &game->img.size.x_exit, &game->img.size.y_exit);
+	game->img.collect = mlx_xpm_file_to_image(game->mlx, coin, &game->img.size.x_collect, &game->img.size.y_collect);
 	game->img.player = mlx_xpm_file_to_image (game->mlx, avatar, &(game->img.size.x_player), &(game->img.size.y_player));
 }
 
@@ -43,13 +40,16 @@ void	create_map(t_game *game)
 			if (game->map.map[y][x] == '1')
 				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.wall, (game->img.size.x_wall * x), (game->img.size.y_wall * y));
 			else if (game->map.map[y][x] == '0')
-				mlx_put_image_to_window(game->mlx, game->mlx_win, game->img.floor, (game->img.width * x), (game->img.height * y));
-			else if (game->map.map[y][x] == 'E')
-					mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.exit, (game->img.width * x), (game->img.height * y));
+				mlx_put_image_to_window(game->mlx, game->mlx_win, game->img.floor, (game->img.size.x_floor * x), (game->img.size.y_floor * y));
 			else if (game->map.map[y][x] == 'C')
-					mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.collect, (game->img.width * x), (game->img.height * y));
-			else if (game->map.map[y][x] == 'P')
-					mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.player, (game->img.size.x_player * y), (game->img.size.y_player * x));
+				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.collect, (game->img.size.x_collect * x), (game->img.size.y_collect * y));
+				//mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.p, (game->img.size.x_player * y), (game->img.size.y_player * x));
+			else if (game->map.map[y][x] == 'E')
+					mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.exit, (game->img.size.x_exit * x), (game->img.size.y_exit * y));
+			//else if (game->map.map[y][x] == 'P')
+		///	{
+				// podemos darle el nombre de las variables de y y x en el momento qu empujamos la imagen al mapa 	
+			//}
 			x++;
 		}
 		y++;
