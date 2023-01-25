24d847b344b4f8ce6a9365f85d96b1c16e80f025
Duvan Gonzalez <dugonzal@c3r3s6.42urduliz.com>
2023-01-25 10:57:18 +0100

    creo una variable para recorrer el mapa
    creo una variable para recorrer el mapa porque de la otra manera al
    limpiar el mapa da error de alojamiento de memoria o algo parecido

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 16:31:18 by ciclo             #+#    #+#             */
-/*   Updated: 2023/01/25 10:46:21 by dugonzal         ###   ########.fr       */
+/*   Updated: 2023/01/25 10:55:56 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,14 @@ void	errors(char *str)
 
 void clean_map(t_game *game)
 {
-    while (game->map.map)
+    int y;
+
+    y = 0;
+    while (game->map.map[y])
     {
-        if (game->map.map)
-            free (game->map.map);
-        game->map.map++;
+        if (game->map.map[y])
+            free (game->map.map[y]);
+        y++;
     }
 }
 
@@ -33,7 +36,7 @@ int	close_window(t_game *game)
 {
 	mlx_destroy_window (game->mlx, game->mlx_win);
 	//antes de salir del programa vamos a liberar la memoria reservada para el mapa
-	clean_map (game);
+    clean_map (game);
 	exit(EXIT_SUCCESS);
 }
 
