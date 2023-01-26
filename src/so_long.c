f78e9af6538b34242b3c570f8ac7411d94779924
dugonzal <ciclocontinuo2@gmail.comm>
2023-01-27 00:44:27 +0100

    intento recorrer la matrix para encotrar las posiciones del jugador pero por alguna razon no sale la imagen

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 14:21:01 by ciclo             #+#    #+#             */
-/*   Updated: 2023/01/27 00:01:45 by dugonzal         ###   ########.fr       */
+/*   Updated: 2023/01/27 00:36:53 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,29 @@ void events(t_game *game)
 	mlx_hook(game->mlx_win, 2, 1, key_press, game);
 }
 
+/*
+void	get_posicion(t_game *game)
+{
+	int y;
+	int x;
 
+	y = 0;
+	while (game->map.map[y] )
+	{
+		x = 0;
+		while (game->map.map[y][x] != 0)
+		{
+			if (game->map.map[y][x] == 'P')
+			{
+				printf ("%d %d", y, x);
+				//game->player.x =  x;
+				ft_printf ("hehehe");
+			}
+				
+		}
+		y++;	
+	}
+}*/
 
 int	main(int ac, char **av)
 {
@@ -45,6 +67,7 @@ int	main(int ac, char **av)
 	game.mlx_win = mlx_new_window(game.mlx, game.map.width * 32 - 28, game.map.height * 32, "so_long");
 	events (&game);
 	get_images (&game); 
+	//get_posicion (&game);
 	mlx_loop(game.mlx);
 	exit (EXIT_SUCCESS);
 } 
