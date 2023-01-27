7ce8bf294e5987ea25c5228b4edc4806f3d2dfc7
dugonzal <ciclocontinuo2@gmail.comm>
2023-01-27 09:47:32 +0100

    creo una funcion para pasar las imagenes a la estructura

--- src/get_images.c
+++ src/get_images.c
@@ -6,24 +6,29 @@
 /*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 08:57:56 by dugonzal          #+#    #+#             */
-/*   Updated: 2023/01/27 00:37:17 by dugonzal         ###   ########.fr       */
+/*   Updated: 2023/01/27 09:46:41 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
 
-void	get_images(t_game *game)
+static void	get_xpm(t_game *game)
 {
 	game->img.player = mlx_xpm_file_to_image(game->mlx, avatar, &(game->img.size.x_avatar), &(game->img.size.y_avatar));
 	game->img.wall = mlx_xpm_file_to_image (game->mlx, walls, &(game->img.size.x_wall), &(game->img.size.y_wall));
 	game->img.floor = mlx_xpm_file_to_image(game->mlx, floors, &(game->img.size.x_floor), &(game->img.size.y_floor));
 	game->img.exit = mlx_xpm_file_to_image (game->mlx, exit_e, &(game->img.size.x_portal), &(game->img.size.y_portal));
-	game->img.collects = mlx_xpm_file_to_image (game->mlx, collect, &(game->img.size.x_collect), &(game->img.size.y_collect));
+	game->img.collects = mlx_xpm_file_to_image (game->mlx, collect, &(game->img.size.x_collect), &(game->img.size.y_collect));	
+}
+
+void	get_images(t_game *game)
+{
 	int y;
 	int x;
 
 	y = 0;
+	get_xpm(game);
 	while (game->map.map[y] != 0)
 	{
 		x = 0;
@@ -38,11 +43,7 @@ void	get_images(t_game *game)
 			else if (game->map.map[y][x] == 'C')
 				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.collects, (game->img.size.x_collect * x), (game->img.size.y_collect * y));
 			else if (game->map.map[y][x] == 'P')
-			{
 				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.player, (game->img.size.x_avatar * x), (game->img.size.y_avatar * y));
-				game->player.x = x;
-				game->player.y = y;
-			}
 			x++;
 		}
 		y++;
