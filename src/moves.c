3edb3527d7f5fd4b481ac0ea6e6a18145658b223
dugonzal <ciclocontinuo2@gmail.com>
2023-01-28 23:29:28 +0200

    estoy reimpriendo el mapa en cada movimiento porque si no lo hago, al minimizar la pantalla se borra los pixeles y al volvert a presionar un evento de teclado se restaura porque se imprime nuevamente

--- src/moves.c
+++ src/moves.c
@@ -6,32 +6,11 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:15:15 by dugonzal          #+#    #+#             */
-/*   Updated: 2023/01/28 23:03:58 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/01/28 23:28:04 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
-
-static void	get(t_game *game)
-{
-	int y;
-	int x;
-
-	y = 0;
-	while (game->map.map[y] != 0)
-	{
-		x = 0;
-		while (game->map.map[y][x] != 0)
-		{
-			if (game->map.map[y][x] == '0')
-                  mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.floor, (x * 32), (y * 32));
-			else if (game->map.map[y][x] == 'P')
-				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.player, (x * 32), (y * 32));
-			x++;
-		}
-		y++;
-	}
-}
 
 void	mov_w(t_game *game)
 {
@@ -42,8 +21,8 @@ void	mov_w(t_game *game)
         game->map.map[game->player.y][game->player.x] = '0';
         game->map.map[game->player.y - 1][game->player.x] = 'P';
         game->player.y--;
-        get (game);
-        printf ("moves -> %d\n", game->player.moves++);
+        get_images (game);
+        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
     }
     if (game->map.map[game->player.y - 1][game->player.x] == 'E' && game->player.collectibles == 0)
         win (game);
@@ -59,8 +38,8 @@ void mov_s(t_game *game)
        game->map.map[game->player.y][game->player.x] = 'P';
        if (game->map.map[game->player.y - 1][game->player.x] != 'E')
             game->map.map[game->player.y - 1][game->player.x] = '0';
-        get (game);
-        printf ("moves -> %d\n", game->player.moves++);
+        get_images (game);
+        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
     }
     else if (game->map.map[game->player.y + 1][game->player.x] == 'E' && game->player.collectibles == 0)
         win (game);
@@ -76,9 +55,8 @@ int mov_a(t_game *game)
        game->map.map[game->player.y][game->player.x] = 'P';
        if (game->map.map[game->player.y][game->player.x] != 'E')
         game->map.map[game->player.y ][game->player.x + 1] = '0';
-       get (game);
-        printf ("moves -> %d\n", game->player.moves++);
-
+       get_images (game);
+        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
     }
     else if (game->map.map[game->player.y][game->player.x - 1] == 'E' && game->player.collectibles == 0)
         win (game);
@@ -95,9 +73,8 @@ void mov_d(t_game *game)
        game->map.map[game->player.y][game->player.x] = 'P';
        if (game->map.map[game->player.y][game->player.x] != 'E')
          game->map.map[game->player.y ][game->player.x - 1] = '0';
-       get (game);
-        printf ("moves -> %d\n", game->player.moves++);
-
+       get_images (game);
+        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
     }
     else if (game->map.map[game->player.y][game->player.x + 1] == 'E' && game->player.collectibles == 0)
         win (game);
