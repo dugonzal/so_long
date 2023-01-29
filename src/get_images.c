9c6a760359e39af3b58b8c56d88420cde86636b0
dugonzal <ciclocontinuo2@gmail.com>
2023-01-29 21:18:24 +0200

    update norminette

--- src/get_images.c
+++ src/get_images.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 08:57:56 by dugonzal          #+#    #+#             */
-/*   Updated: 2023/01/28 23:06:55 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/01/29 21:09:14 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	check_xpm(t_game *game)
 {
-	if (!game->img.player || !game->img.wall || !game->img.floor \
-	|| !game->img.exit || !game->img.collects )
+	if (!game->img.player || !game->img.wall || \
+	!game->img.floor || !game->img.exit || !game->img.collects)
 		errors("XPM not found");
 }
 
 static void	get_xpm(t_game *game)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	game->img.player = mlx_xpm_file_to_image(game->mlx, avatar, &(x), &(y));
 	game->img.wall = mlx_xpm_file_to_image (game->mlx, walls, &(x), &(y));
@@ -30,6 +30,25 @@ static void	get_xpm(t_game *game)
 	game->img.exit = mlx_xpm_file_to_image (game->mlx, exit_e, &(x), &(y));
 	game->img.collects = mlx_xpm_file_to_image (game->mlx, collect, &(x), &(y));
 	check_xpm(game);
+}
+
+static void	put_images(t_game *game, int x, int y)
+{
+	if (game->map.map[y][x] == '1')
+		mlx_put_image_to_window (game->mlx, game->mlx_win, \
+		game->img.wall, (x * 32), (y * 32));
+	else if (game->map.map[y][x] == '0')
+		mlx_put_image_to_window (game->mlx, game->mlx_win, \
+			game->img.floor, (x * 32), (y * 32));
+	else if (game->map.map[y][x] == 'E')
+		mlx_put_image_to_window (game->mlx, game->mlx_win, \
+			game->img.exit, (x * 32), (y * 32));
+	else if (game->map.map[y][x] == 'C')
+		mlx_put_image_to_window (game->mlx, game->mlx_win, \
+			game->img.collects, (x * 32), (y * 32));
+	else if (game->map.map[y][x] == 'P')
+		mlx_put_image_to_window (game->mlx, game->mlx_win, \
+			game->img.player, (x * 32), (y * 32));
 }
 
 void	get_images(t_game *game)
@@ -44,16 +63,7 @@ void	get_images(t_game *game)
 		x = 0;
 		while (game->map.map[y][x] != 0)
 		{
-			if (game->map.map[y][x] == '1')
-				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.wall, (x * 32), (y * 32));
-			else if (game->map.map[y][x] == '0')
-				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.floor, (x * 32), (y * 32));
-			else if (game->map.map[y][x] == 'E')
-				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.exit, (x * 32), (y * 32));
-			else if (game->map.map[y][x] == 'C')
-				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.collects, (x * 32), (y * 32));
-			else if (game->map.map[y][x] == 'P')
-				mlx_put_image_to_window (game->mlx, game->mlx_win, game->img.player, (x * 32), (y * 32));
+			put_images(game, x, y);
 			x++;
 		}
 		y++;
--- src/moves.c
+++ src/moves.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:15:15 by dugonzal          #+#    #+#             */
-/*   Updated: 2023/01/29 15:14:07 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/01/29 21:18:05 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,78 +14,77 @@
 
 void	mov_w(t_game *game)
 {
-    if (game->map.map[game->player.y - 1][game->player.x] != '1' \
-    && game->map.map[game->player.y - 1][game->player.x] != 'E')
-    {
-        if (game->map.map[game->player.y - 1][game->player.x] == 'C')
-            game->player.collectibles--;
-        game->map.map[game->player.y][game->player.x] = '0';
-        game->map.map[game->player.y - 1][game->player.x] = 'P';
-        game->player.y--;
-        get_images (game);
-        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
-    }
-    if (game->map.map[game->player.y - 1][game->player.x] == 'E' \
-    && game->player.collectibles == 0)
-        win (game);
+	if (game->map.map[game->player.y - 1][game->player.x] != '1' \
+	&& game->map.map[game->player.y - 1][game->player.x] != 'E')
+	{
+		if (game->map.map[game->player.y - 1][game->player.x] == 'C')
+			game->player.collectibles--;
+		game->map.map[game->player.y][game->player.x] = '0';
+		game->map.map[game->player.y - 1][game->player.x] = 'P';
+		game->player.y--;
+		get_images (game);
+		printf (RED"moves -> %d\n"RESET, game->player.moves++);
+	}
+	if (game->map.map[game->player.y - 1][game->player.x] == 'E' \
+	&& game->player.collectibles == 0)
+		win (game);
 }
 
-void mov_s(t_game *game)
+void	mov_s(t_game *game)
 {
-    if (game->map.map[game->player.y + 1][game->player.x] != '1' \
-    && game->map.map[game->player.y + 1][game->player.x] != 'E')
-    {
-        if (game->map.map[game->player.y + 1][game->player.x] == 'C')
-            game->player.collectibles--;
-       game->player.y++;
-       game->map.map[game->player.y][game->player.x] = 'P';
-       if (game->map.map[game->player.y - 1][game->player.x] != 'E')
-            game->map.map[game->player.y - 1][game->player.x] = '0';
-        get_images (game);
-        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
-    }
-    else if (game->map.map[game->player.y + 1][game->player.x] == 'E' \
-    && game->player.collectibles == 0)
-        win (game);
+	if (game->map.map[game->player.y + 1][game->player.x] != '1' \
+	&& game->map.map[game->player.y + 1][game->player.x] != 'E')
+	{
+		if (game->map.map[game->player.y + 1][game->player.x] == 'C')
+			game->player.collectibles--;
+		game->player.y++;
+		game->map.map[game->player.y][game->player.x] = 'P';
+		if (game->map.map[game->player.y - 1][game->player.x] != 'E')
+		game->map.map[game->player.y - 1][game->player.x] = '0';
+		get_images (game);
+		printf (CYAN"moves -> %d\n"RESET, game->player.moves++);
+	}
+	else if (game->map.map[game->player.y + 1][game->player.x] == 'E' \
+	&& game->player.collectibles == 0)
+		win (game);
 }
 
-int mov_a(t_game *game)
+void	mov_a(t_game *game)
 {
-    if (game->map.map[game->player.y][game->player.x - 1] != '1' \
-    && game->map.map[game->player.y][game->player.x - 1] != 'E')
-    {
-        if (game->map.map[game->player.y][game->player.x - 1] == 'C')
-            game->player.collectibles--;
-       game->player.x--;
-       game->map.map[game->player.y][game->player.x] = 'P';
-       if (game->map.map[game->player.y][game->player.x] != 'E')
-        game->map.map[game->player.y ][game->player.x + 1] = '0';
-       get_images (game);
-        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
-    }
-    else if (game->map.map[game->player.y][game->player.x - 1] == 'E' \
-    && game->player.collectibles == 0)
-        win (game);
-    return 0;
+	if (game->map.map[game->player.y][game->player.x - 1] != '1' \
+	&& game->map.map[game->player.y][game->player.x - 1] != 'E')
+	{
+		if (game->map.map[game->player.y][game->player.x - 1] == 'C')
+			game->player.collectibles--;
+		game->player.x--;
+		game->map.map[game->player.y][game->player.x] = 'P';
+		if (game->map.map[game->player.y][game->player.x] != 'E')
+			game->map.map[game->player.y][game->player.x + 1] = '0';
+		get_images (game);
+		printf (GREY"moves -> %d\n"RESET, game->player.moves++);
+	}
+	else if (game->map.map[game->player.y][game->player.x - 1] == 'E' \
+	&& game->player.collectibles == 0)
+		win (game);
 }
 
-void mov_d(t_game *game)
+void	mov_d(t_game *game)
 {
-    if (game->map.map[game->player.y][game->player.x + 1] != '1' \
-    && game->map.map[game->player.y][game->player.x + 1] != 'E')
-    {
-        if (game->map.map[game->player.y][game->player.x + 1] == 'C')
-            game->player.collectibles--;
-       game->player.x++;
-       game->map.map[game->player.y][game->player.x] = 'P';
-       if (game->map.map[game->player.y][game->player.x] != 'E')
-         game->map.map[game->player.y ][game->player.x - 1] = '0';
-       get_images (game);
-        printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
-    }
-    else if (game->map.map[game->player.y][game->player.x + 1] == 'E' \
-    && game->player.collectibles == 0)
-        win (game);
+	if (game->map.map[game->player.y][game->player.x + 1] != '1' \
+	&& game->map.map[game->player.y][game->player.x + 1] != 'E')
+	{
+		if (game->map.map[game->player.y][game->player.x + 1] == 'C')
+			game->player.collectibles--;
+		game->player.x++;
+		game->map.map[game->player.y][game->player.x] = 'P';
+		if (game->map.map[game->player.y][game->player.x] != 'E')
+			game->map.map[game->player.y][game->player.x - 1] = '0';
+		get_images (game);
+		printf (GREEN"moves -> %d\n"RESET, game->player.moves++);
+	}
+	else if (game->map.map[game->player.y][game->player.x + 1] == 'E' \
+	&& game->player.collectibles == 0)
+		win (game);
 }
 
 /// @brief controlador de los eventos de teclado
@@ -96,13 +95,13 @@ int	key_press(int keycode, t_game *game)
 {
 	if (keycode == Q || keycode == ESC)
 		close_window(game);
-    else if (keycode == W)
-       mov_w (game);
-    else if (keycode == D)
-        mov_d(game);
-    else if (keycode == A)
-        mov_a(game);
-    else if (keycode == S)
-        mov_s (game);
-    return (0);
+	else if (keycode == W)
+		mov_w (game);
+	else if (keycode == D)
+		mov_d(game);
+	else if (keycode == A)
+		mov_a(game);
+	else if (keycode == S)
+		mov_s (game);
+	return (0);
 }
