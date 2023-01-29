157e608d7196b52f6df7bbb18e75d958c8fbc170
dugonzal <ciclocontinuo2@gmail.com>
2023-01-29 17:01:01 +0200

    update norminette

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: ciclo <ciclo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 16:31:18 by ciclo             #+#    #+#             */
-/*   Updated: 2023/01/29 15:43:05 by ciclo            ###   ########.fr       */
+/*   Updated: 2023/01/29 16:58:06 by ciclo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,43 +18,42 @@ void	errors(char *str)
 	exit (1);
 }
 
-void clean_map(t_game *game)
+void	clean_map(t_game *game)
 {
-    int y;
+	int y;
 
-    y = 0;
-    while (game->map.map[y])
-    {
-        if (game->map.map[y])
-            free (game->map.map[y]);
-        y++;
-    }
+	y = 0;
+	while (game->map.map[y])
+	{
+		if (game->map.map[y])
+			free (game->map.map[y]);
+		y++;
+	}
 }
 
-//antes de salir del programa vamos a liberar la memoria reservada para el mapa
 int	close_window(t_game *game)
 {
 	mlx_destroy_window (game->mlx, game->mlx_win);
-    mlx_destroy_image (game->mlx, game->img.exit);
-    mlx_destroy_image (game->mlx, game->img.collects);
-    mlx_destroy_image (game->mlx, game->img.floor);
-    mlx_destroy_image (game->mlx ,game->img.player);
-    clean_map (game);
+	mlx_destroy_image (game->mlx, game->img.exit);
+	mlx_destroy_image (game->mlx, game->img.collects);
+	mlx_destroy_image (game->mlx, game->img.floor);
+	mlx_destroy_image (game->mlx ,game->img.player);
+	clean_map (game);
 	exit(EXIT_SUCCESS);
 }
 
-void    win(t_game *game)
+void	win(t_game *game)
 {
-    ft_printf(GREEN"\n\
-            ██████████████████████████████████████████████████████████████████\n\
-            ██                                                              ██\n\
-            ██  ███  ███  ██████  ██    ██     ██      ██ ██ ██    ██   ██  ██\n\
-            ██   ██  ██  ██    ██ ██    ██     ██      ██ ██ ███   ██   ██  ██\n\
-            ██    ████   █      █ ██    ██     ██  ██  ██ ██ ██ ██ ██   ██  ██\n\
-            ██     ██    ██    ██ ██    ██     ██ ████ ██ ██ ██  ████       ██\n\
-            ██     ██     ██████    ████        ███  ███  ██ ██   ███   ██  ██\n\
-            ██                                                              ██\n\
-            ██████████████████████████████████████████████████████████████████\n\
-            \n"RESET);
-    close_window(game);
+		ft_printf(GREEN"\n\
+		██████████████████████████████████████████████████████████████████\n\
+		██                                                              ██\n\
+		██  ███  ███  ██████  ██    ██     ██      ██ ██ ██    ██   ██  ██\n\
+		██   ██  ██  ██    ██ ██    ██     ██      ██ ██ ███   ██   ██  ██\n\
+		██    ████   █      █ ██    ██     ██  ██  ██ ██ ██ ██ ██   ██  ██\n\
+		██     ██    ██    ██ ██    ██     ██ ████ ██ ██ ██  ████       ██\n\
+		██     ██     ██████    ████        ███  ███  ██ ██   ███   ██  ██\n\
+		██                                                              ██\n\
+		██████████████████████████████████████████████████████████████████\n\
+	\n"RESET);
+	close_window(game);
 }
